4b0b68fe186f923dc894d66ee920e195eaef21ba
s529215 <42754268+s529215@users.noreply.github.com>
2020-02-13 12:36:49 -0600

    adding  javascript code

---
+++ javascript/calculate.js
@@ -0,0 +1,11 @@
+function calculate(){
+    const total = parseFloat(document.getElementById("total").value);
+    const rate = parseFloat(document.getElementById("rate").value);
+    const tip = (total*rate)/100;
+    const tax = (total*0.055);
+    const gtotal = (total + (tip) + (tax));
+    document.getElementById("tip").innerHTML = '$' + tip;
+    document.getElementById("tax").innerHTML = '$' + tax.toFixed(2);
+    document.getElementById("gtotal").innerHTML = '$' + gtotal.toFixed(2);
+    return false;
+}
