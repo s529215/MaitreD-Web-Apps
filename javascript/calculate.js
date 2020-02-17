50d8e342c2f0300d6b5d0ae4273a1225ee7b7eaf
s529215 <42754268+s529215@users.noreply.github.com>
2020-02-17 14:09:34 -0600

    fixing nan issue

--- javascript/calculate.js
+++ javascript/calculate.js
@@ -4,6 +4,10 @@ function calculate(){
     const tip = (total*rate)/100;
     const tax = (total*0.055);
     const gtotal = (total + (tip) + (tax));
+    if(isNaN(tip)){
+        tip = null;
+    }
+
     document.getElementById("tip").innerHTML = '$' + tip;
     document.getElementById("tax").innerHTML = '$' + tax.toFixed(2);
     document.getElementById("gtotal").innerHTML = '$' + gtotal.toFixed(2);
